a0cc288728d8607a8c049d94ebdd3440cee30217
Gimesi Gergely <giemsigergely02@gmail.com>
2024-10-26 14:15:47 +0200

    Added a role for Companies

--- Bulky.DataAccess/Migrations/20241026111527_addCompanyRecords.Designer.cs
+++ Bulky.DataAccess/Migrations/20241026111527_addCompanyRecords.Designer.cs
@@ -4,6 +4,7 @@ using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bulky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241026111527_addCompanyRecords")]
+    partial class addCompanyRecords
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -87,7 +90,7 @@ namespace Bulky.DataAccess.Migrations
                     b.Property<string>("State")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("StreetAddress")
+                    b.Property<string>("StreetAdress")
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
@@ -103,7 +106,7 @@ namespace Bulky.DataAccess.Migrations
                             PhoneNumber = "6669990000",
                             PostalCode = "12111",
                             State = "IL",
-                            StreetAddress = "Tech St"
+                            StreetAdress = "Tech St"
                         },
                         new
                         {
@@ -113,7 +116,7 @@ namespace Bulky.DataAccess.Migrations
                             PhoneNumber = "7779990000",
                             PostalCode = "666666",
                             State = "IL",
-                            StreetAddress = "999 Vid St"
+                            StreetAdress = "999 Vid St"
                         },
                         new
                         {
@@ -123,7 +126,7 @@ namespace Bulky.DataAccess.Migrations
                             PhoneNumber = "1113335555",
                             PostalCode = "99999",
                             State = "NY",
-                            StreetAddress = "999 Main St"
+                            StreetAdress = "999 Main St"
                         });
                 });
 
@@ -477,9 +480,6 @@ namespace Bulky.DataAccess.Migrations
                     b.Property<string>("City")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<int?>("CompanyId")
-                        .HasColumnType("int");
-
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
@@ -492,8 +492,6 @@ namespace Bulky.DataAccess.Migrations
 
                     b.Property<string>("StreetAddress")
                         .HasColumnType("nvarchar(max)");
-
-                    b.HasIndex("CompanyId");
 
                     b.HasDiscriminator().HasValue("ApplicationUser");
                 });
@@ -558,15 +556,6 @@ namespace Bulky.DataAccess.Migrations
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
-                });
-
-            modelBuilder.Entity("Bulky.Models.ApplicationUser", b =>
-                {
-                    b.HasOne("Bulky.Models.Company", "Company")
-                        .WithMany()
-                        .HasForeignKey("CompanyId");
-
-                    b.Navigation("Company");
                 });
 #pragma warning restore 612, 618
         }
